c26ce5e78b4b01c21c44bde775c6802c31185a02
keremdkmc <kerem.dkmc98@outlook.com>
2020-07-13 22:26:20 +0300

    Login ve sginup

---
+++ WebAdvert.Web/Models/Accounts/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.CognitoIdentityProvider.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class LoginModel
+    {
+        [Display(Name ="Email")]
+        [Required(ErrorMessage ="This field is required")]
+        [EmailAddress]
+        public string Email{ get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
